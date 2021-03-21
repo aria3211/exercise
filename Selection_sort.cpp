6b9f6d4691105f238461a2f8369164e7c4b4e9f0
aira_32 <79927472+aira3211@users.noreply.github.com>
2021-03-21 20:42:09 +0330

    Update Selection_sort.cpp

--- Selection_sort.cpp
+++ Selection_sort.cpp
@@ -1,22 +1,54 @@
 # include <iostream>
+
 using namespace std;
 
 
-
-
+#define MAX 100
 
 int main()
 {
-  int a[8] = {1,2,6,8,9,3,4,5};
-  int index_arr = 0;
-  cout << "Enter an integer in the list to search for: ";
-  cin >> index_arr;
+  int arr[MAX];
+  int n,i,j;
+  int temp;
+  cout << "Enter a len array: " << '\n';
+  cin >> n;
 
-  for (int i = 0; i<sizeof(a)/sizeof(a[0]); ++i ){
-    if (index_arr == a[i]){
-      cout <<  i ;
+  // check output
+  if (n<0||n>MAX)
+  {
+    cout << "Error valid range!!"<<'\n';
+    return -1;
+  }
+  for (i=0;i<n;i++)
+  {
+    
+    cout << "Enter element["<< i+1 <<"]";
+    cin >> arr[i];
+  }
+
+  cout << "Unsorted Array elements: "<<endl;
+  for(i=0;i<n;++i)
+  {
+    cout << arr[i]<<'\t';
+  }
+  cout << '\n';
+  cout << "Sorting...."<<'\n';
+  for(i=0;i<n;i++){
+    for (j=i+1;j<n;j++){
+      if (arr[i]>arr[j]){
+        temp = arr[i];
+        arr[i]=arr[j];
+        arr[j] = temp;
+      }
     }
+  }
+
+  for (i=0;i<n;i++){
+    cout << arr[i]<<'\t';
+  }cout << endl;
+
+  return 0;
+
 
 }
 
-}
